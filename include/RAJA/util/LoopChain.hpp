8d87ec15ed478323a0540aff9ab31ffb50935cb6
brandonneth <nethbrandone@gmail.com>
2020-03-17 19:08:19 -0700

    code for figuring out the fusion on a pair of for alls

--- include/RAJA/util/LoopChain.hpp
+++ include/RAJA/util/LoopChain.hpp
@@ -20,7 +20,13 @@ namespace RAJA
 {
 struct SymAccess;
 
-int can_fuse(std::vector<SymAccess> const & a1, std::vector<SymAccess> const & a2);
+int can_fuse_1d(std::vector<SymAccess> const & a1, std::vector<SymAccess> const & a2);
+int can_fuse_2d(std::vector<SymAccess> const & a1, std::vector<SymAccess> const & a2);
+int amount_to_shift(std::vector<SymAccess> const & a1, std::vector<SymAccess> const & a2);
+
+
+
+
 struct SymIter {
     std::string name;
     int idx;
@@ -87,12 +93,11 @@ struct SymAccess {
     SymAccess(void * v, std::vector<SymIter>& is) {
         view = v;
         iters = is;
-        type = "UNSET";
-
+        isRead = 0;
+        isWrite = 0;
     }
 
     void set_read() {
-        type = "READ";
         isRead = 1;
     }
 
@@ -307,8 +312,8 @@ struct KernelW {
 
     std::vector<SymAccess> allAccesses = std::vector<SymAccess>();
 
-    for(int i = 0; i < accesses1->size(); i++) {allAccesses.push_back(accesses1->at(i));}
-    for(int i = 0; i < accesses2->size(); i++) {allAccesses.push_back(accesses2->at(i));}
+    for(long unsigned int i = 0; i < accesses1->size(); i++) {allAccesses.push_back(accesses1->at(i));}
+    for(long unsigned int i = 0; i < accesses2->size(); i++) {allAccesses.push_back(accesses2->at(i));}
     return allAccesses;
   }
 
@@ -337,29 +342,24 @@ KernelW<PolicyType,SegmentTuple,Bodies...> makeKernel(SegmentTuple const & segme
 }
 
 template <typename PolicyType, typename SegmentTuple, typename... Bodies1, typename... Bodies2>
-auto fuseKernels(
+auto fuse_kernels(
    KernelW<PolicyType,SegmentTuple,Bodies1...> knl1,
    KernelW<PolicyType,SegmentTuple,Bodies2...> knl2)
 {
   //std::cout << "fusing kernels\n";
   
-  static bool canFuse = 1;
-  auto bodies1 = knl1.bodies;
-   auto bodies2 = knl2.bodies;
+  //static bool canFuse = 1;
     
     
-  auto lambda1 = std::get<0>(bodies1);
-  auto lambda2 = std::get<0>(bodies2);
-  auto newlambda = [=] (auto i, auto j) {
-     lambda1(i,j);
-     lambda2(i,j);
+  static auto newlambda = [=] (auto i, auto j) {
+     std::get<0>(knl1.bodies)(i,j);
+     std::get<0>(knl2.bodies)(i,j);
   };
 
-  auto newKernel = makeKernel<PolicyType>(knl1.segments,newlambda);
+  static auto newKernel = makeKernel<PolicyType>(knl1.segments,newlambda);
   
   //std::cout << "\nexecuting fused kernel from within fuseKernels\n";
 
-  newKernel();
   return newKernel;
 }
 
@@ -376,7 +376,7 @@ template <typename ExecPolicy,
           typename LoopBody>
 struct ForAll {
 
-    const Container &segment;
+    Container segment;
     LoopBody func;
     constexpr ForAll(Container const & c, LoopBody const & l) :
         segment((c)),
@@ -411,10 +411,45 @@ struct ForAll {
 
 template <typename ExecPolicy, typename Container, typename LoopBody>
 ForAll<ExecPolicy,Container,LoopBody> makeForAll(Container const & c, LoopBody const & l) {
+
     return ForAll<ExecPolicy,Container,LoopBody>(
         (c),
         (l));
 }
+
+template <typename ExecPolicy, typename Container, typename LoopBody1>
+auto shift_forall(ForAll<ExecPolicy,Container,LoopBody1> knl, const int & amount) {
+   
+   auto shifted_func = [=](auto i) {knl.func(i-amount);};
+   
+   auto segment = knl.segment;
+   
+   long int lb = *segment.begin();
+   long int ub = *segment.end();
+   
+   auto shifted_lb = lb + amount;
+   auto shifted_ub = ub + amount;
+   auto shifted_segment = RAJA::RangeSegment(shifted_lb,shifted_ub); 
+  
+   return RAJA::makeForAll<ExecPolicy>(RAJA::RangeSegment(shifted_lb,shifted_ub),shifted_func);
+
+}
+
+
+template <typename ExecPolicy, typename Container, typename Func1, typename Func2>
+auto fuse_unequal_bounds(RAJA::ForAll<ExecPolicy,Container,Func1> forall1, RAJA::ForAll<ExecPolicy,Container,Func2> forall2) {
+   auto l1 = *forall1.segment.begin();
+   auto u1 = *forall1.segment.end();
+ 
+   auto l2 = *forall2.segment.begin();
+   auto u2 = *forall2.segment.end();
+  
+   
+
+
+}
+
+
 
 
 template <typename ExecPolicy,
@@ -436,7 +471,7 @@ void chain(ForAll<ExecPolicy,Container,Func1> forall1, RAJA::ForAll<ExecPolicy,C
     static bool fusable = can_fuse(forall1.execute_symbolically(), forall2.execute_symbolically());
 
     if (fusable) {
-        auto newForAll = fuse(forall1, forall2);
+        static auto newForAll = fuse(forall1, forall2);
         chain(newForAll, std::forward<Args>(args)...);
     } else {
         forall1();
@@ -449,6 +484,115 @@ template <typename ExecPolicy, typename Container, typename Func1, typename...Ar
 void chain(ForAll<ExecPolicy,Container,Func1> forall1) {
     forall1();
 }
+
+
+std::string read_string(std::vector<SymAccess> a1, std::vector<SymAccess> a2, int dimensions);
+std::string write_string(std::vector<SymAccess> a1, std::vector<SymAccess> a2, int dimensions);
+
+isl_stat print_point_val(isl_point * pnt, void * user);
+isl_stat collect_point_val(isl_point * pnt, void * user);
+
+template <typename ExecPol, typename Container, typename Func1, typename Func2>
+std::vector<int> amount_to_shift_no_nesting(ForAll<ExecPol,Container,Func1> forall1, ForAll<ExecPol,Container,Func2> forall2) {
+   
+  auto a1 = forall1.execute_symbolically();
+  auto a2 = forall2.execute_symbolically();
+
+  auto readString = read_string(a1,a2,1);//.c_str();
+  auto writeString = write_string(a1,a2,1);//.c_str();
+ 
+  std::cout << "Calculating shift amount\n";
+  std::cout << "read string: " << readString << "\n";
+  std::cout << "write string: "<< writeString << "\n";
+  
+  isl_ctx* ctx = isl_ctx_alloc();
+  isl_printer * p = isl_printer_to_file(ctx, stdout);
+
+  const char * domainString = "[l0,u0] -> { L1[i] : i < u0 and i >= l0; L2[i] : i < u0 and i >= l0;}";
+  isl_union_set * domain = isl_union_set_read_from_str(ctx,domainString);
+
+  
+  isl_union_map * reads = isl_union_map_read_from_str(ctx, readString.c_str());
+  isl_union_map * writes = isl_union_map_read_from_str(ctx, writeString.c_str());
+  
+
+  //reads = isl_union_map_intersect_domain(reads, isl_union_set_copy(domain));
+  //writes = isl_union_map_intersect_domain(writes, isl_union_set_copy(domain));
+
+  isl_union_map * reads_inverse = isl_union_map_reverse(isl_union_map_copy(reads));
+  isl_union_map * writes_inverse = isl_union_map_reverse(isl_union_map_copy(writes));
+  std::cout << "\nreads map\n";
+  p = isl_printer_print_union_map(p, reads);
+
+  std::cout << "\nwrites map\n";
+  p = isl_printer_print_union_map(p, writes);
+
+  std::cout << "\nreads map inverse \n";
+  p = isl_printer_print_union_map(p, reads_inverse);
+
+  std::cout << "\nwrites map inverse \n";
+  p = isl_printer_print_union_map(p, writes_inverse);
+
+  isl_union_map * raw = isl_union_map_apply_range(isl_union_map_copy(writes), isl_union_map_copy(reads_inverse));
+  isl_union_map * waw = isl_union_map_apply_range(isl_union_map_copy(writes), isl_union_map_copy(writes_inverse));
+  isl_union_map * war = isl_union_map_apply_range(isl_union_map_copy(reads), isl_union_map_copy(writes_inverse));
+
+  std::cout << "\nraw\n";
+  p = isl_printer_print_union_map(p, raw);
+
+  std::cout << "\nwar\n";
+  p = isl_printer_print_union_map(p, war);
+
+  std::cout << "\nwaw\n";
+  p = isl_printer_print_union_map(p, waw);
+
+  isl_union_map * deps = isl_union_map_union(raw, waw);
+  deps = isl_union_map_union(deps, war);
+
+  std::cout << "\ndeps\n";
+  p = isl_printer_print_union_map(p,deps);
+  
+  isl_union_set * l1_0 = isl_union_set_read_from_str(ctx, "{L1[0]}");
+
+  std::cout << "\nL1 iteration 0 set\n";
+  p = isl_printer_print_union_set(p,l1_0);
+ 
+  isl_union_set * depVectors = isl_union_set_apply(l1_0, deps);
+
+  std::cout << "\ndep vectors\n";
+  p = isl_printer_print_union_set(p,depVectors);  
+
+  std::vector<int> * l1_to_l2_deps = new std::vector<int>();
+  
+  isl_union_set * l2Points = isl_union_set_read_from_str(ctx, "{L2[n]}");
+
+  isl_union_set * l1_to_l2_set = isl_union_set_intersect(isl_union_set_copy(depVectors), l2Points);
+  std::cout << "\nl1 to l2 set\n";
+  p = isl_printer_print_union_set(p,l1_to_l2_set);
+ 
+  isl_set * set =  isl_set_from_union_set(l1_to_l2_set);
+
+  std::cout <<"\nl1 to l2 as a set set\n";
+  p = isl_printer_print_set(p, set);
+ 
+  isl_set_foreach_point(set, collect_point_val, (void*) l1_to_l2_deps);
+
+  
+  std::vector<int> depVals = *l1_to_l2_deps;  
+
+  delete l1_to_l2_deps;
+
+  int minAmount = 0;
+
+  for(int val : depVals) {
+    if(val < minAmount) {
+      minAmount = val;
+    }
+  } 
+ 
+   std::cout << "\n";
+   return std::vector<int>{0,minAmount};
+} //amount_to_shift_no_nesting
 } //namespace RAJA
 
 #endif

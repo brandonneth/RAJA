9eb96b5bdceba122e5be8db3d533a2a42802e240
brandonneth <nethbrandone@gmail.com>
2020-08-24 12:16:24 -0700

    moved definitions of chain and grouped kernel ojbects into their own file, Chain

---
+++ include/RAJA/loopchain/Chain.hpp
@@ -0,0 +1,113 @@
+// Contains definitions for the types that are returned by fuse functions and such
+
+
+#ifndef RAJA_Chain_HPP
+#define RAJA_Chain_HPP
+
+#include "RAJA/config.hpp"
+
+
+namespace RAJA
+{
+
+// Encodes the result of grouping together a sequence of perfectly nested 
+// kernels. If there were originally n d-dimensional kernels, their associated 
+// GroupedKernels object will have 2*n*d + 1 kernels in it. Executing a 
+// GroupedKernels object will execute the kernels in the order they were 
+// provided to the constructor. MainKnlIdx is the index of the kernel whose 
+// iteration space is the intersection of all the iteration spaces of the 
+// original kernels. Currently, this index value is the middle value in the 
+// sequence.
+template <camp::idx_t MainKnlIdx, typename... KernelTypes>
+struct GroupedKernels {
+  using KernelTuple = camp::tuple<KernelTypes...>;
+
+  const KernelTuple knlTuple;
+
+  static constexpr camp::idx_t numKnls = sizeof...(KernelTypes);
+
+  GroupedKernels(const KernelTuple & knlTuple_) : knlTuple(knlTuple_) {};
+
+  // returns a tuple of kernels that would execute before the main kernel
+  auto pre_knls() {
+    return tuple_slice<0,MainKnlIdx>(knlTuple);
+  }
+
+  // returns a tuple of kernels that would execute after the main kernel
+  auto post_knls() {
+    return tuple_slice<MainKnlIdx+1, numKnls>(knlTuple);
+  }
+
+  // returns the kernel that executes over the shared iteration space
+  auto main_knl() {
+    return camp::get<MainKnlIdx>(knlTuple);
+  }
+
+  // executes each kernel in the object one at a time
+  template <camp::idx_t I, camp::idx_t...Is>  
+  void execute(camp::idx_seq<I, Is...>) {
+    camp::get<I>(knlTuple)();
+    if constexpr (sizeof...(Is) == 0) {
+      return;
+    } else {
+      execute(camp::idx_seq<Is...>{});
+    }
+  }
+
+  RAJA_INLINE
+  void operator() () {
+    auto seq = camp::make_idx_seq_t<numKnls>{};
+    execute(seq);
+  }
+
+}; //GroupedKernels
+
+
+// constructor function for GroupedKernels that takes a tuple of kernels
+template <camp::idx_t MainKnlIdx, typename...KernelTypes>
+auto grouped_kernels(camp::tuple<KernelTypes...> knlTuple) {
+  return GroupedKernels<MainKnlIdx, KernelTypes...>(knlTuple);
+}
+
+// constructor function for GroupedKernels that takes kernels
+template <camp::idx_t MainKnlIdx, typename...KernelTypes>
+auto grouped_kernels(KernelTypes... knls) {
+  return grouped_kernels<MainKnlIdx>(camp::make_tuple(knls...));
+}
+
+
+// Encodes a sequence of loops. Executing a LoopChain object executes each 
+//  kernel in the chain one at a time.
+template <typename... KernelTypes>
+struct LoopChain {
+  using KernelTuple = camp::tuple<KernelTypes...>;
+  
+  const KernelTuple knlTuple;
+  
+  LoopChain(const KernelTuple & knlTuple_) : knlTuple(knlTuple_) {}
+
+  template <camp::idx_t I, camp::idx_t...Is>
+  void execute(camp::idx_seq<I,Is...>) {
+    camp::get<I>(knlTuple)();
+    if constexpr (sizeof...(Is) == 0) {
+      return;
+    } else {
+      execute(camp::idx_seq<Is...>{});
+    }
+  }
+}; //LoopChain
+
+// LoopChain constructor function for kernel tuple
+template <typename... KernelTypes>
+auto loop_chain(camp::tuple<Knls...> knlTuple) {
+  return LoopChain<KernelTypes...>(knlTuple);
+}
+
+// LoopChain constructor function for kernels
+template <typename...KernelTypes>
+auto loop_chain(KernelTypes...knls) {
+  return LoopChain(KernelTypes...>(camp::make_tuple(knls...));
+}
+
+} //namespace RAJA
+#endif

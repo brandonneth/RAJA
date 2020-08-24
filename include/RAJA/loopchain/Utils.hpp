ac6294c8160a7a40e561a2729e7dba15821c8ef2
brandonneth <nethbrandone@gmail.com>
2020-08-24 13:05:58 -0700

    cleaned up camp utils, removed a lot of the more obscure ones until I'm sure I need tehm.

--- include/RAJA/loopchain/Utils.hpp
+++ include/RAJA/loopchain/Utils.hpp
@@ -2,169 +2,99 @@
 #ifndef RAJA_LoopChainUtils_HPP
 #define RAJA_LoopChainUtils_HPP
 
+
+
 namespace RAJA
 {
 
-RAJA_INLINE
-auto tuple_cat(auto tuple1, auto tuple2) {
-  return camp::tuple_cat_pair(tuple1, tuple2);
-}
-template <typename...T>
-RAJA_INLINE
-auto tuple_cat(auto tuple1, T&&...tuples) {
-  return camp::tuple_cat_pair(tuple1, tuple_cat(std::forward<T>(tuples)...));
-}
+// camp::idx_seq utility functions
+//   - idx_seq_cat
+//   - idx_seq_shift
+//   - idx_seq_for
+//   - idx_seq_from_to
 
-template <camp::idx_t amount, camp::idx_t...Seq>
-RAJA_INLINE
-auto add_to_idx_seq(camp::idx_seq<Seq...>) {
-  return camp::idx_seq<(Seq+amount)...>{};
-
-}
-template <camp::idx_t start, camp::idx_t end>
-RAJA_INLINE
-auto idx_seq_from_to() {
-  auto zeroSeq = camp::make_idx_seq_t<end-start>{};
-
-  return add_to_idx_seq<start>(zeroSeq);
-}
-
-template <camp::idx_t...UpTo, camp::idx_t...After>
-RAJA_INLINE
-auto replace_index_with_helper(auto originalTuple, auto replacementTuple, camp::idx_seq<UpTo...>, camp::idx_seq<After...>) {
-
-
-  auto upToTuple = camp::make_tuple(camp::get<UpTo>(originalTuple)...);
-
-  auto afterTuple = camp::make_tuple(camp::get<After>(originalTuple)...);
-
-  auto tempTuple = camp::tuple_cat_pair(upToTuple, replacementTuple);
-
-  return camp::tuple_cat_pair(tempTuple, afterTuple);
-}
-
-
-template <camp::idx_t ToReplace>
-RAJA_INLINE
-auto replace_index_with(auto originalTuple, auto replacementTuple) {
-
-  constexpr auto size = camp::tuple_size<decltype(originalTuple)>().value;
-
-  auto upToSeq = idx_seq_from_to<0,ToReplace>();
-  auto afterSeq = idx_seq_from_to<ToReplace+1, size>();
-
-
-  return replace_index_with_helper(originalTuple, replacementTuple, upToSeq, afterSeq);;
-
-}
-
-template <camp::idx_t... UpTo, camp::idx_t...After>
-RAJA_INLINE
-auto remove_index_helper(auto originalTuple, camp::idx_seq<UpTo...>, camp::idx_seq<After...>) {
-
-  auto upToTuple = camp::make_tuple(camp::get<UpTo>(originalTuple)...);
-
-  auto afterTuple = camp::make_tuple(camp::get<After>(originalTuple)...);
-
-  return camp::tuple_cat_pair(upToTuple, afterTuple);
-
-}
-
-
-template <camp::idx_t ToRemove>
-RAJA_INLINE
-auto remove_index(auto originalTuple) {
-  constexpr auto size = camp::tuple_size<decltype(originalTuple)>().value;
-
-  auto upToSeq = idx_seq_from_to<0,ToRemove>();
-  auto afterSeq = idx_seq_from_to<ToRemove+1, size>();
-
-  return remove_index_helper(originalTuple, upToSeq, afterSeq);
-}
-
-
-
-template <camp::idx_t... Is>
-RAJA_INLINE
-auto slice_tuple(auto tuple, camp::idx_seq<Is...>) {
-  return make_tuple(camp::get<Is>(tuple)...);
-}
-
-
-template <camp::idx_t start, camp::idx_t end> 
-RAJA_INLINE
-auto slice_tuple(auto tuple) {
-
-  if constexpr (start == end) {
-    return make_tuple();
-  } else {
-    auto seq = idx_seq_from_to<start,end>();
-
-    return slice_tuple(tuple, seq);
-  }
-}
-
-template <typename...Args>
-RAJA_INLINE
-auto idx_seq_for(camp::tuple<Args...>) {
-
-  return camp::make_idx_seq_t<sizeof...(Args)>{};
-}
+// idx_seq_cat: Concatenates an arbitrary number of sequences
 
 template <camp::idx_t...Is, camp::idx_t...Js>
-RAJA_INLINE
 auto idx_seq_cat(camp::idx_seq<Is...>, camp::idx_seq<Js...>) {
   return camp::idx_seq<Is...,Js...>{};
-}//idx_seq_cat
+}
+
+template <camp::idx_t...Is, typename...Seqs>
+auto idx_seq_cat(camp::idx_seq<Is...> s1, Seqs&&...seqs) {
+  return idx_seq_cat(s1, idx_seq_cat(std::forward<Seqs>(seqs)...));
+}
+
+// idx_seq_shift: Shifts each value in the sequence by the amount
+
+template <camp::idx_t amount, camp::idx_t...Is>
+auto idx_seq_shift(camp::idx_seq<Is...>) {
+  return camp::idx_seq<(Is+amount)...>{};
+}
+
+// idx_seq_for: Returns the idx seq for a tuple
+
+template <typename... ElemTypes>
+auto idx_seq_for(camp::tuple<ElemTypes...> t) {
+  return camp::make_idx_seq_t<sizeof...(ElemTypes)>{};
+}
+
+// idx_seq_from_to: Returns an idx seq from the start value up to the end value
+
+template <camp::idx_t start, camp::idx_t end>
+auto idx_seq_from_to() {
+  auto zeroSeq = camp::make_idx_seq_t<end-start>{};
+  
+  return idx_seq_shift<start>(zeroSeq);
+}
 
 
-template <camp::idx_t I>
-RAJA_INLINE
-auto tuple_repeat(auto repeated) {
-  if constexpr (I <= 0) {
 
+
+
+// camp::tuple utility functions
+//  - tuple_cat
+//  - tuple_slice
+//  - tuple_len 
+
+
+// tuple_cat: Concatenates an arbitrary number of tuples
+
+template <typename Tuple1, typename Tuple2>
+auto tuple_cat(Tuple1 t1, Tuple2 t2) {
+  return camp::tuple_cat_pair(t1,t2);
+}
+
+template <typename Tuple1, typename...Tuples>
+auto tuple_cat(Tuple1 t1, Tuples&&...tuples) {
+  return tuple_cat(t1, tuple_cat(std::forward<Tuples>(tuples)...));
+}
+
+// tuple_slice: Returns a slice of a tuple, with [) bounds as template arguments or with a idx sequence
+
+template <typename Tuple, camp::idx_t...Is>
+auto tuple_slice(Tuple t, camp::idx_seq<Is...>) {
+  return make_tuple(camp::get<Is>(t)...);
+}
+
+template <camp::idx_t start, camp::idx_t end, typename Tuple>
+auto tuple_slice(Tuple t) {
+  if constexpr (start >= end) {
     return make_tuple();
   } else {
-    return tuple_cat(make_tuple(repeated), tuple_repeat<I-1>(repeated));
+    return tuple_slice(t, idx_seq_from_to<start,end>());
   }
 }
 
-
-template <typename T>
-RAJA_INLINE
-auto tuple_repeat(auto repeated) {
-  return make_tuple(repeated);
-}
-
-template <typename T, typename... Ts> 
-RAJA_INLINE
-auto tuple_repeat(auto repeated) {
-  auto subTuple = tuple_repeat<Ts...>(repeated);
-
-  return tuple_cat(subTuple, make_tuple(repeated));
-}
-
-
-template <typename T>
-RAJA_INLINE
-auto flatten_tuple(camp::tuple<T> tupleOfTuples) {
-  return camp::get<0>(tupleOfTuples);
-}
-
-template <typename T, typename...Ts>
-RAJA_INLINE
-auto flatten_tuple(camp::tuple<T,Ts...> tupleOfTuples) {
-   
-   return tuple_cat(camp::get<0>(tupleOfTuples), flatten_tuple(remove_index<0>(tupleOfTuples)));
-  
-
-}
-
+// tuple_len: Returns the length of a tuple
 template <typename...Ts>
 RAJA_INLINE
 constexpr auto tuple_len(camp::tuple<Ts...>) {
   return sizeof...(Ts);
 }
+
+
+
 } //namespace RAJA
 
 #endif

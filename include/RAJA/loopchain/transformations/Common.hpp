050cd8875482d940f67320c5df82bf522a675c99
brandonneth <nethbrandone@gmail.com>
2020-09-02 14:05:12 -0700

    untested implementation of segment tuple intersection

--- include/RAJA/loopchain/transformations/Common.hpp
+++ include/RAJA/loopchain/transformations/Common.hpp
@@ -29,11 +29,32 @@ auto intersect_segments(camp::tuple<SegmentTypes...> segments, camp::idx_seq<Is.
   return RangeSegment(highestLow, lowestHigh);
 }
 
+template <typename...SegmentTypes>
+auto intersect_segments(camp::tuple<SegmentTypes...> segments) {
+  return intersect_segments(segments, idx_seq_for(segments));
+}
+
+template <typename...SegmentTupleTypes, camp::idx_t...NumTuples, camp::idx_t...NumDims>
+auto intersect_segment_tuples(camp::tuple<SegmentTupleTypes...> segmentTuples, 
+                              camp::idx_seq<NumTuples...>,
+                              camp::idx_seq<NumDims...>) {
+
+  //for n in numDims
+    //nth of each
+  auto zeroethDims = make_tuple((camp::get<0>(camp::get<NumTuples>(segmentTuples))...));
+
+  auto groupedByDim =  make_tuple((make_tuple((camp::get<NumDims>(camp::get<NumTuples>(segmentTuples))...)))...);
+
+  return make_tuple(intersect_segments(camp::get<NumDims>(groupedByDim))...);
+}
+
+
+
 template <typename...SegmentTupleTypes, camp::idx_t...Is>
 auto intersect_segment_tuples(camp::tuple<SegmentTupleTypes...> segmentTuples, camp::idx_seq<Is...> seq) {
-  
-  
+  constexpr numDims = tuple_len(camp::get<0>(segmentTuples));
 
+  return intersect_segment_tuples(segmentTuples, seq, idx_seq_for(camp::get<0>(segmentTuples)));
 }
 // For an arbitrary number of segment tuples of the same dimensionality,
 // returns a segment tuple for the intersection of the segment tuples.
